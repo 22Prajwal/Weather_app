ca2f225bb30ef731efc555d701e15652232c5f4b
PRAJWAL P KURDEKAR <prajwalpkurdekar522@gmail.com>
2023-09-07 16:01:46 +0530

    Created using react.js
    APIs Used
    Open Weather APIs
    
    https://openweathermap.org/current
    
    API Info
    Method: GET
    URL: https://api.openweathermap.org/data/2.5/weather?q={CITY_NAME}&appid={API_KEY}

---
+++ weathers/weather-app/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Weather from './Weather';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <Weather/>
+     
+    
+    </div>
+  );
+}
+
+export default App;
